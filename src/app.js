3823fbe0f7a9b4fd369f6b643a710a5b13fc0702
Siianko <tsiyanko@gmail.com>
2018-06-20 17:01:01 +0200

    add multiple contacts and fixed all the errors

--- src/app.js
+++ src/app.js
@@ -1,76 +1,70 @@
-const storage = window.localStorage
-
 const renderContacts = () => {
-    // Read all the contacts from the storage
-    const contacts = JSON.parse(storage.getItem('contacts'))
-  
-    // Select the container we will use to list the contacts 
-    let div = document.querySelector('.contact-list')
-  
-    if (contacts) {
+  const storage = window.localStorage
+  const contacts = JSON.parse(storage.getItem('contacts'))
+  let div = document.querySelector('.contact-list')
+  if (contacts) {
       div.innerHTML = ''
-  
-      // render the contacts
       const ul = document.createElement('ul')
-  
-      // For every contact in our array of contacts, we will
-      // create a li element that will contain a card with
-      // all the information of the contact
       contacts.forEach(contact => {
-        let li = document.createElement('li')
-        li.innerHTML = `
-          <div class="card">
-            <div class="image">
-              <img src="https://ca-address-book.herokuapp.com/images/pine.jpg" />
-            </div>
-            <div class="content">
-              <h1>${ contact.name }</h1>
-              <h2>${ contact.company }</h2>
-              <p>${ contact.notes }</p> 
-              ${ contact.email } | 
-              <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
-            </div>
-          </div>
-       `
-        // Add the contact's li to the unordered list we created earlier
-        ul.appendChild(li)
+          let li = document.createElement('li')
+          li.innerHTML = `
+              <div class='card'>
+                  <div class='image'> 
+                      <img src='https://ca-address-book.herokuapp.com/images/pine.jpg' alt='Pines' height='175' width='263'/>
+                  </div>
+                  <div class='content'>
+                      <h1>${ contact.name }</h1>
+                      <h2>${ contact.company }</h2>
+                      <p>${ contact.notes }</p>
+                      ${ contact.email } |
+                      <a href='https//www.twitter.com/${ contact.twitter}'>@${ contact.twitter}</a>
+                  </div>
+              </div>    
+          `
+          ul.appendChild(li)
       })
-  
-      // Lastly, append the list to the contact-list container.
-      div.appendChild(ul) 
-    } else { 
-      div.innerHTML = '<p>You have no contacts in your address book</p>' 
-    }
+  div.appendChild(ul)
+  }   else {
+      div.innerHTML = '<p>You have no contacts in your address book</p>'
   }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderContacts()
+    const addContactForm = document.querySelector('.new-contact-form')
   
-document.addEventListener('DOMContetnLoaded', () => {
-    renderContacts()
-    const contactForm = document.querySelector('.new-contact-form')
-
-    contactForm.addEventListener('submit', event => {
-        event.preventDefault()
-
-        const {
-            name,
-            email,
-            phone,
-            company,
-            notes,
-            twitter,
-        } = addContactForm.elements
-
-        const contact = {
-            id: Date.now(),
-            name: name.value,
-            email: email.value,
-            phone: phone.value,
-            company: company.value,
-            notes: notes.value,
-            twitter: twitter.value,
-        }
-
-    storage.setItem('contacts', JSON.stringify([contact]))
-    renderContacts()
-    contactForm.reset()
+    // Register an event to listen for form submission
+    addContactForm.addEventListener('submit', event => {
+      event.preventDefault()
+      const storage = window.localStorage
+  
+      // Get all inputs elements from the form
+      const {
+        name,
+        email,
+        phone,
+        company,
+        notes,
+        twitter,
+      } = addContactForm.elements
+  
+      // Create contact object
+      const contact = {
+        id: Date.now(),
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+        company: company.value,
+        notes: notes.value,
+        twitter: twitter.value,
+      }
+  
+      console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
+      let contacts = JSON.parse(storage.getItem('contacts')) || []
+      contacts.push(contact)
+      storage.setItem('contacts', JSON.stringify(contacts))
+      renderContacts()
+      addContactForm.reset()
+    
     })
-})
+  })
